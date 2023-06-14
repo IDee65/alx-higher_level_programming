3ae74f1a1b65cb0c394191b2acb8ee47df8156d3
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 22:49:21 +0100

    only diff

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """build set of elements exclusive to one of two sets
+
+    Args:
+        set_1 (set): first set
+        set_2 (set): second set
+
+    Returns:
+        set: elements in only one set
+    """
+    return set_1 ^ set_2
