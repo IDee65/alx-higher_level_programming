1dd461b8fa3834c5f0e1d6efb96805223be77f2f
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 23:00:58 +0100

    mult by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    """Multiply all values in dictionary by 2
+
+    Args:
+        a_dictionary (Any): dictionary
+
+    Returns:
+        dictionary: Dictionary with values multiplied by 2
+    """
+    return {key: a_dictionary[key] * 2 for key in a_dictionary}
