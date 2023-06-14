c6613ae1eba0ab273f70f6d7e2071c8d828895fc
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 22:46:14 +0100

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """perform intersect operation on two sets
+
+    Args:
+        set_1 (set): first set
+        set_2 (set): second set
+
+    Returns:
+        set: intersect of first and second set
+    """
+    return set_1 & set_2
