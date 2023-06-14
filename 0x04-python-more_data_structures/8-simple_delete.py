98275083205d445330e99bb755859738f53de95c
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 22:59:33 +0100

    simple del

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    """Delete key value in dictionary
+
+    Args:
+        a_dictionary (Any): dictionary
+        key (str, optional): key. Defaults to "".
+
+    Returns:
+        dictionary: dictionary with deletedd key value pair
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
