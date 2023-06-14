4c64c756a6cb42bdc1c7efc3bd2016eaba23cd56
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 22:58:05 +0100

    update

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Update dictionary
+
+    Args:
+        a_dictionary (dictionary): dictionary
+        key: key
+        value (Any): value
+
+    Returns:
+        dictionary: updated dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
