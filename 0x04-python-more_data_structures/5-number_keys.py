001c1679a18f2e740407fbb859f1150044d2c997
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-06-14 22:51:55 +0100

    num keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """get number of keys in dictionary
+
+    Args:
+        a_dictionary (dictionary): dictionary
+
+    Returns:
+        number: number of keys in dictionary
+    """
+    return len(a_dictionary)
