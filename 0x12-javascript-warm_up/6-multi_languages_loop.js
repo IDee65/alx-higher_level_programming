91757a7ae3cc07f2c8681f2f360b9e244d62616f
Idemudia <idemudia.agbonghae@iisysgroup.com>
2023-09-12 07:47:08 +0100

    multi_lang_loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const stringArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < stringArr.length; i++) {
+  console.log(stringArr[i]);
+}
